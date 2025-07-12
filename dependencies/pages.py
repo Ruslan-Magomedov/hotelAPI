9546d6a4c552ab6c5d4916bfdf04040a2c33f389
Ruslan Magomedov <mail@mail.ru>
2025-07-12 17:14:20 +0300

    add paginations for data & pydantic schemes

---
+++ dependencies/pages.py
@@ -0,0 +1,12 @@
+from fastapi import Depends, Query
+from pydantic import BaseModel
+
+from typing import Annotated
+
+
+class Pagenation(BaseModel):
+    page: Annotated[int, Query(default=1, ge=1)]
+    quantity: Annotated[int, Query(default= 3, ge=3, le=10)]
+
+
+PagenationDep = Annotated[Pagenation, Depends()]
--- hotels.py
+++ hotels.py
@@ -1,6 +1,11 @@
-from fastapi import FastAPI, APIRouter, Query, Body
+from fastapi import FastAPI, APIRouter, Body
+
+from typing import Annotated
 
 import check_id
+from dependencies.pages import PagenationDep
+from openapi_examples import EX_DATA
+from schema.hotels import Hotel, HotelPatch
 
 
 router = APIRouter(prefix="/hotels", tags=["Отели"])
@@ -11,15 +16,21 @@ data = [
     {"id": 4, "title": "Sochi", "name": "Miramar"},
     {"id": 5, "title": "Moscow", "name": "Octopus"},
     {"id": 6, "title": "Moscow", "name": "Laina"},
+    {"id": 7, "title": "Rim", "name": "Sora"},
+    {"id": 8, "title": "NY", "name": "Star"},
+    {"id": 9, "title": "Chana", "name": "laka"},
+    {"id": 10, "title": "Sochi", "name": "Miramar"},
+    {"id": 11, "title": "Moscow", "name": "Octopus"},
+    {"id": 12, "title": "Moscow", "name": "Laina"},
 ]
 
 
 @router.get("")
-def get_all_hotels():
+def get_all_hotels(page: PagenationDep):
     """
     Ручка для получения всех отелей
     """
-    return data
+    return data[page.quantity * (page.page-1):][:page.quantity]
 
 
 @router.delete("/{hotel_id}")
@@ -35,40 +46,40 @@ def delete_hotel_by_id(hotel_id: int):
 
 
 @router.post("")
-def create_hotel(title: str, name: str):
+def create_hotel(hotel: Hotel = Body(openapi_examples=EX_DATA)):
     """
     Ручка для создания отеля
     """
-    if type(title) == str and type(name) == str:
+    if type(hotel.title) == str and type(hotel.name) == str:
         new_id = data[-1]["id"] + 1
-        data.append({"id": new_id, "title": title, "name": name})
+        data.append({"id": new_id, "title": hotel.title, "name": hotel.name})
         return {"message": "OK"}
     return {"message": "Bad Data"}
 
 
 @router.put("/{hotel_id}")
-def change_hotel_by_id(hotel_id: int, title: str = Body(), name: str = Body()):
+def change_hotel_by_id(hotel_id: int, hotel: Hotel = Body(openapi_examples=EX_DATA)):
     """
     Ручка для изменения отеля по id
     """
     index, check_bool = check_id.check(data, hotel_id)
     if check_bool:
-        data[index]["title"] = title
-        data[index]["name"] = name
+        data[index]["title"] = hotel.title
+        data[index]["name"] = hotel.name
         return {"message": "OK"}
     return {"message": f"Not Found by id {hotel_id}"}
 
 
 @router.patch("/{hotel_id}")
-def change_hotel_by_id_patch(hotel_id: int, title: str | None = Body(None), name: str | None = Body(None)):
+def change_hotel_by_id_patch(hotel_id: int, hotel: HotelPatch = Body(openapi_examples=EX_DATA)):
     """
     Ручка для изменения отеля по id (частично)
     """
     index, check_bool = check_id.check(data, hotel_id)
     if check_bool:
-        if title != None:
-            data[index]["title"] = title
-        if name != None:
-            data[index]["name"] = name
+        if hotel.title != None:
+            data[index]["title"] = hotel.title
+        if hotel.name != None:
+            data[index]["name"] = hotel.name
         return {"message": "OK"}
     return {"message": f"Not Found by id {hotel_id}"}
