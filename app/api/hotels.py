30a7cac6557c9395d83bf9dbe8a1d1345631837e
Ruslan Magomedov <mail@mail.ru>
2025-07-19 04:42:04 +0300

    create tables hotels & rooms

--- app/api/hotels.py
+++ app/api/hotels.py
@@ -1,73 +1,63 @@
 from fastapi import APIRouter, Body
+from sqlalchemy import insert
 
 import app.dependencies.check_id as check_id
 from app.dependencies.pages import PagenationDep
 from app.openapi_examples import EX_DATA
 from app.schema.hotels import Hotel, HotelPatch
 
+from app.db import async_session_maker
+from app.models.hotels import HotelsOrm
+
 
 router = APIRouter(prefix="/hotels", tags=["Отели"])
-data = [
-    {"id": 1, "title": "Rim", "name": "Sora"},
-    {"id": 2, "title": "NY", "name": "Star"},
-    {"id": 3, "title": "Chana", "name": "laka"},
-    {"id": 4, "title": "Sochi", "name": "Miramar"},
-    {"id": 5, "title": "Moscow", "name": "Octopus"},
-    {"id": 6, "title": "Moscow", "name": "Laina"},
-    {"id": 7, "title": "Rim", "name": "Sora"},
-    {"id": 8, "title": "NY", "name": "Star"},
-    {"id": 9, "title": "Chana", "name": "laka"},
-    {"id": 10, "title": "Sochi", "name": "Miramar"},
-    {"id": 11, "title": "Moscow", "name": "Octopus"},
-    {"id": 12, "title": "Moscow", "name": "Laina"},
-]
 
 
-@router.get("")
-def get_all_hotels(page: PagenationDep):
-    """Ручка для получения всех отелей c пагинацией"""
-    return data[page.quantity * (page.page-1):][:page.quantity]
-
-
-@router.delete("/{hotel_id}")
-def delete_hotel_by_id(hotel_id: int):
-    """Ручка для удаления отеля по id"""
-    index, check_bool = check_id.check(data, hotel_id)
-    if check_bool:
-        data.pop(index)
-        return data
-    return {"message": f"Not Found by id {hotel_id}"}
+# @router.get("")
+# def get_all_hotels(page: PagenationDep):
+#     """Ручка для получения всех отелей c пагинацией"""
+#     return data[page.quantity * (page.page-1):][:page.quantity]
 
 
 @router.post("")
-def create_hotel(hotel: Hotel = Body(openapi_examples=EX_DATA)):
+async def create_hotel(hotel: Hotel = Body(openapi_examples=EX_DATA)):
     """Ручка для создания отеля"""
-    if type(hotel.title) == str and type(hotel.name) == str:
-        new_id = data[-1]["id"] + 1
-        data.append({"id": new_id, "title": hotel.title, "name": hotel.name})
-        return {"message": "OK"}
-    return {"message": "Bad Data"}
+    async with async_session_maker() as session:
+        SQL_QUERY = insert(HotelsOrm).values(**hotel.model_dump())
+        await session.execute(SQL_QUERY)
+        await session.commit()
+    return {"message": "hotel created"}
 
 
-@router.put("/{hotel_id}")
-def change_hotel_by_id(hotel_id: int, hotel: Hotel = Body(openapi_examples=EX_DATA)):
-    """Ручка для изменения отеля по id"""
-    index, check_bool = check_id.check(data, hotel_id)
-    if check_bool:
-        data[index]["title"] = hotel.title
-        data[index]["name"] = hotel.name
-        return {"message": "OK"}
-    return {"message": f"Not Found by id {hotel_id}"}
+# @router.delete("/{hotel_id}")
+# def delete_hotel_by_id(hotel_id: int):
+#     """Ручка для удаления отеля по id"""
+#     index, check_bool = check_id.check(data, hotel_id)
+#     if check_bool:
+#         data.pop(index)
+#         return data
+#     return {"message": f"Not Found by id {hotel_id}"}
 
 
-@router.patch("/{hotel_id}")
-def change_hotel_by_id_patch(hotel_id: int, hotel: HotelPatch = Body(openapi_examples=EX_DATA)):
-    """Ручка для изменения отеля по id (частично)"""
-    index, check_bool = check_id.check(data, hotel_id)
-    if check_bool:
-        if hotel.title != None:
-            data[index]["title"] = hotel.title
-        if hotel.name != None:
-            data[index]["name"] = hotel.name
-        return {"message": "OK"}
-    return {"message": f"Not Found by id {hotel_id}"}
+# @router.put("/{hotel_id}")
+# def change_hotel_by_id(hotel_id: int, hotel: Hotel = Body(openapi_examples=EX_DATA)):
+#     """Ручка для изменения отеля по id"""
+#     index, check_bool = check_id.check(data, hotel_id)
+#     if check_bool:
+#         data[index]["title"] = hotel.title
+#         data[index]["name"] = hotel.name
+#         return {"message": "OK"}
+#     return {"message": f"Not Found by id {hotel_id}"}
+
+
+# @router.patch("/{hotel_id}")
+# def change_hotel_by_id_patch(hotel_id: int, hotel: HotelPatch = Body(openapi_examples=EX_DATA)):
+#     """Ручка для изменения отеля по id (частично)"""
+#     index, check_bool = check_id.check(data, hotel_id)
+#     if check_bool:
+#         if hotel.title != None:
+#             data[index]["title"] = hotel.title
+#         if hotel.name != None:
+#             data[index]["name"] = hotel.name
+#         return {"message": "OK"}
+#     return {"message": f"Not Found by id {hotel_id}"}
